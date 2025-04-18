d4bf94d80522f32d3afe00572f9e57e001544be5
losyash <losyash@mail.ru>
2025-04-18 17:43:11 +0300

    04.18:17-43

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -4,8 +4,6 @@ import (
 	"testing"
 )
 
-// TestHelloName calls greetings.Hello with a name, checking
-// for a valid return value.
 func TestSumm(t *testing.T) {
 	ans := summ(2, -2)
 	if ans != 0 {
